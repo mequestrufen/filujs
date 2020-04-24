cc7e8d47f80fd7704489534222a5ca3dd1fab3d8
Felipe Gómez <fgomez@thoughtworks.com>
2020-04-24 16:16:13 -0400

    collision detection added, score counter, some fixes

--- filu.js
+++ filu.js
@@ -1,20 +1,34 @@
 var canvas;
-var squareSize = 20;
+var squareSize = 30;
 var backgroundColor = '#070707';
+var interval;
+
+var boundarySize = 20;
 
 var direction = 'S';
-var filuSize = 7;
+var filuSize = 3;
 var head = {
-    x: 7,
-    y: 7    
+    x: 10,
+    y: 10   
+};
+
+var target = {
+    x: 0,
+    y: 0
 };
 
 var filu = []
 
 document.addEventListener('keydown', keyDown);
 
-function draw() {
+function setCanvas(canvasElement) {
+    canvas.fillStyle = backgroundColor;
+    canvas.fillRect(0, 0, canvasElement.width, canvasElement.height);
 
+    canvas.font = '48px menlo';
+}
+
+function draw() {
     switch (direction) {
         case 'N':
             head.y--;            
@@ -39,7 +53,7 @@ function draw() {
     };
 
     filu.push(item);    
-    drawSquare(head.x, head.y);     
+    drawSquare(head.x, head.y, 'gray');     
     
     if(filu.length > filuSize) {
         eraseSquare(filu[0].x, filu[0].y)
@@ -47,8 +61,8 @@ function draw() {
     }
 }
 
-function drawSquare(x, y) {
-    canvas.fillStyle = 'rgb(200, 200, 200)';
+function drawSquare(x, y, color) {
+    canvas.fillStyle = color;
     canvas.fillRect(x * squareSize, y * squareSize, squareSize, squareSize);
 
     canvas.fillStyle = backgroundColor;
@@ -60,36 +74,93 @@ function eraseSquare(x, y) {
     canvas.fillRect(x * squareSize, y * squareSize, squareSize, squareSize);
 }
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function drawTarget() {
+    do {
+        target.x = getRandomInt(20);
+        target.y = getRandomInt(20);
+    } while (isInFilu());
+    drawSquare(target.x, target.y, 'green');
+}
+
+function isInFilu(x, y) {
+    filu.forEach((item) => {
+        if(item.x === target.x && item.y === target.y) {
+            return true;
+        }
+    })
+    return false;
+}
+
+function log(x, y) {
+    console.log('x: ' + x + ' / y: ' + y);
+}
+
+function hitFilu() {
+    //detect self inflictment   
+    let dupl = 0; 
+    filu.forEach((item) => {
+        if(item.x === head.x && item.y === head.y) {
+            dupl++;
+        }
+    });
+    return (dupl === 2);    
+}
+
+function hitBoundary() {
+    return (head.x < 0 || head.x > boundarySize -1 || head.y < 0 || head.y > boundarySize -1);
+}
+
+function targetHit() {
+    if(head.x === target.x && head.y === target.y) {
+        filuSize++;
+        drawTarget();
+    }    
+}
+
 function keyDown(e) {
     switch(e.code) {
         case 'ArrowUp':
-            direction = 'N';
+            if (direction !== 'S') direction = 'N';
             break;
 
         case 'ArrowDown':
-            direction = 'S';
+            if (direction !== 'N') direction = 'S';
             break;
 
         case 'ArrowLeft':
-            direction = 'O';
+            if (direction !== 'E') direction = 'O';            
             break;                        
 
         case 'ArrowRight':
-            direction = 'E'
+            if (direction !== 'O') direction = 'E';
             break;                     
     }
 }
 
 function show(){
+    var sizeElement = document.getElementById('sizeElement');
     var canvasElement = document.getElementById('canvas');
     if (canvasElement.getContext) {
       canvas = canvasElement.getContext('2d');
     }
+    
+    setCanvas(canvasElement);
+    drawTarget();
 
-    canvas.fillStyle = backgroundColor;
-    canvas.fillRect(0, 0, canvasElement.width, canvasElement.height);
-
-    setInterval(() => {
+    interval = setInterval(() => {
         draw();
-    }, 250);    
+        targetHit();
+        sizeElement.innerHTML = filuSize;
+
+        if(hitFilu() || hitBoundary()) {                        
+            clearInterval(interval);
+
+            canvas.fillStyle = 'red';
+            canvas.fillText('Fail!', 10, 50);
+        }
+    }, 115);    
 }   
