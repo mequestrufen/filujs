50842bf5b6ebe12171e9cc0fb63b5fbbbeb056e3
Felipe Gómez <fgomez.meqsystems@gmail.com>
2020-06-03 12:06:43 -0400

    unit tests for movement added

--- __tests__/game.spec.ts
+++ __tests__/game.spec.ts
@@ -1,6 +1,55 @@
 import Game from "../src/game";
 
-test('Initialize the snake with a size of 3', () => {    
+test('Initialize the snake with a size of 1', () => {    
     let game = new Game();
-    expect(game.size).toBe(3);
+    expect(game.size).toBe(1);
 });
+
+test('The snake must start at (10, 10)', () => {
+    let game = new Game();
+    expect(game.head.x).toEqual(10);
+    expect(game.head.y).toEqual(10);
+});
+
+test('The snake should move upwards after one step', () => {
+    let game = new Game();
+    game.step();
+    expect(game.head.y).toEqual(9);
+});
+
+test('The snake should move left when direction changed to West before one step', () => {
+    let game = new Game();
+    game.left();
+    game.step();
+    expect(game.head.x).toEqual(9);
+    expect(game.head.y).toEqual(10);
+});
+
+test('The snake should move right when direction changed to East before one step', () => {
+    let game = new Game();
+    game.right();
+    game.step();
+    expect(game.head.x).toEqual(11);
+    expect(game.head.y).toEqual(10);
+});
+
+test('The snake should move downwards when direction changed to South before one step only if moving sideways', () => {
+    let game = new Game();
+    game.right();
+    game.step();
+    game.down();
+    game.step();
+    expect(game.head.x).toEqual(11);
+    expect(game.head.y).toEqual(11);
+});
+
+test('The snake should move upwards when direction changed to North before one step only if moving sideways', () => {
+    let game = new Game();
+    game.left();
+    game.step();
+    game.up();
+    game.step();
+    expect(game.head.x).toEqual(9);
+    expect(game.head.y).toEqual(9);
+});
+
--- src/game.ts
+++ src/game.ts
@@ -1,6 +1,47 @@
+enum Direction {
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+};
+
 class Game {
-    readonly size = 3;
+    readonly size = 1;
+    readonly head = {x: 10, y: 10, direction: Direction.UP};    
+
+    step() {
+        if (this.head.direction === Direction.LEFT) {
+            this.head.x--;
+        }
+
+        if (this.head.direction === Direction.UP) {
+            this.head.y--;
+        }
+
+        if (this.head.direction === Direction.RIGHT) {
+            this.head.x++;
+        }
+
+        if(this.head.direction === Direction.DOWN) {
+            this.head.y++;
+        }
+    }
+
+    left() {
+        this.head.direction = Direction.LEFT;
+    }
+
+    right() {
+        this.head.direction = Direction.RIGHT;
+    }
+
+    down() {
+        this.head.direction = Direction.DOWN;
+    }
+
+    up() {
+        this.head.direction = Direction.UP;
+    }
 }
 
 export default Game;
-
