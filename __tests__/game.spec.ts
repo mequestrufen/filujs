bde2eb26c183ce4a2fe3544a4392e7a8e2c12f8b
Felipe Gómez <fgomez.meqsystems@gmail.com>
2020-06-04 21:06:26 -0400

    some more tests added. New types and startup of Game defined

--- __tests__/game.spec.ts
+++ __tests__/game.spec.ts
@@ -1,55 +1,76 @@
-import Game from "../src/game";
+import { Game, Direction } from "../src/game";
 
-test('Initialize the snake with a size of 1', () => {    
+test('Initialize the snake with a length of 1', () => {    
     let game = new Game();
-    expect(game.size).toBe(1);
+    expect(game.length).toBe(1);
 });
 
 test('The snake must start at (10, 10)', () => {
     let game = new Game();
     expect(game.head.x).toEqual(10);
     expect(game.head.y).toEqual(10);
+
+    expect(game.head).toEqual({x: 10, y: 10, direction: Direction.UP});
 });
 
-test('The snake should move upwards after one step', () => {
+test('The snake should move upwards three cells after three steps', () => {
     let game = new Game();
     game.step();
-    expect(game.head.y).toEqual(9);
+    game.step();
+    game.step();
+
+    expect(game.head.y).toEqual(7);
 });
 
-test('The snake should move left when direction changed to West before one step', () => {
+test('The snake should move left when direction changed to LEFT before one step', () => {
     let game = new Game();
     game.left();
     game.step();
-    expect(game.head.x).toEqual(9);
-    expect(game.head.y).toEqual(10);
+    
+    expect(game.head).toEqual({x: 9, y: 10, direction: Direction.LEFT});
 });
 
-test('The snake should move right when direction changed to East before one step', () => {
+test('The snake should move right when direction changed to RIGHT before one step', () => {
     let game = new Game();
     game.right();
     game.step();
-    expect(game.head.x).toEqual(11);
-    expect(game.head.y).toEqual(10);
+
+    expect(game.head).toEqual({x: 11, y: 10, direction: Direction.RIGHT});
 });
 
-test('The snake should move downwards when direction changed to South before one step only if moving sideways', () => {
+test('The snake should move downwards when direction changed to DOWN before one step only if moving sideways', () => {
     let game = new Game();
     game.right();
     game.step();
     game.down();
     game.step();
-    expect(game.head.x).toEqual(11);
-    expect(game.head.y).toEqual(11);
+
+    expect(game.head).toEqual({x: 11, y: 11, direction: Direction.DOWN});
 });
 
-test('The snake should move upwards when direction changed to North before one step only if moving sideways', () => {
+test('The snake should move upwards when direction changed to UP before one step only if moving sideways', () => {
     let game = new Game();
     game.left();
     game.step();
     game.up();
     game.step();
-    expect(game.head.x).toEqual(9);
-    expect(game.head.y).toEqual(9);
+
+    expect(game.head).toEqual({x: 9, y: 9, direction: Direction.UP});
 });
 
+test('Food will set a new specific target', () => {
+    let game = new Game();
+    game.food(9, 10);
+
+    expect(game.target).toEqual({x: 9, y: 10});
+});
+
+test('The snake body should grow one cell after getting food', () => {
+    let game = new Game();
+    game.food(10, 9);
+    game.step();
+    game.step();
+
+    expect(game.length).toEqual(2);
+    expect(game.body).toEqual([{x: 10, y: 9}, {x:10, y:8}]);
+});
--- src/game.ts
+++ src/game.ts
@@ -1,47 +1,107 @@
-enum Direction {
+type Head = {
+    x: number,
+    y: number,
+    direction: Direction
+};
+
+export enum Direction {
     LEFT,
     RIGHT,
     UP,
     DOWN
 };
 
-class Game {
-    readonly size = 1;
-    readonly head = {x: 10, y: 10, direction: Direction.UP};    
+class Point {
+    x: number;
+    y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+};
+
+export class Game {    
+    private size = 1;
+    private _head: Head;
+    private _body: Array<Point>;
+    
+    private _target: Point;
+
+    get length(): number {
+        return this._body.length;
+    }
+
+    get head(): Head {
+        return this._head;
+    }
+
+    get body(): Array<Point> {
+        return this._body;
+    }
+
+    get target(): Point {
+        return this._target;
+    }
+
+    constructor() {       
+        let startPoint = new Point(10, 10); 
+        this._body = new Array();
+        this._body.push(startPoint);
+        this._head = {x: startPoint.x, y: startPoint.y, direction: Direction.UP};
+        this._target = new Point(9, 10); //new random target
+    }
 
     step() {
-        if (this.head.direction === Direction.LEFT) {
-            this.head.x--;
+        switch (this._head.direction) {
+            case Direction.UP:
+                this._head.y--;            
+                break;
+    
+            case Direction.DOWN:
+                this._head.y++;
+                break;
+    
+            case Direction.LEFT:
+                this._head.x--;
+                break;
+    
+            case Direction.RIGHT:
+                this._head.x++;
+                break;
         }
 
-        if (this.head.direction === Direction.UP) {
-            this.head.y--;
-        }
+        this._body.push(new Point(this._head.x, this._head.y));
 
-        if (this.head.direction === Direction.RIGHT) {
-            this.head.x++;
+        if(this._body.length > this.size) {
+            //erase square HERE
+            this._body = this._body.slice(1);
         }
+    
+        // Draw square HERE
 
-        if(this.head.direction === Direction.DOWN) {
-            this.head.y++;
+        if (this._head.x === this._target.x && this._head.y === this._target.y) {
+            this.size++;
         }
     }
 
     left() {
-        this.head.direction = Direction.LEFT;
+        this._head.direction = Direction.LEFT;
     }
 
     right() {
-        this.head.direction = Direction.RIGHT;
+        this._head.direction = Direction.RIGHT;
     }
 
     down() {
-        this.head.direction = Direction.DOWN;
+        this._head.direction = Direction.DOWN;
     }
 
     up() {
-        this.head.direction = Direction.UP;
+        this._head.direction = Direction.UP;
     }
-}
 
-export default Game;
+    food(x: number, y: number) {        
+        this._target = new Point(x, y);
+    }
+};
